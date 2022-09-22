0197ffa10765ae9ca4f70604da24a15db7e3560c
vldurov <wow.4ik@icloud.com>
2022-09-22 08:47:25 +0300

    added hw_part2

--- scripts/index.js
+++ scripts/index.js
@@ -7,9 +7,25 @@ document.write(styles.join("&harr;") + "&#10148;" + "Добавить элеме
 styles.splice(1,1,"Классика");
 document.write(styles.join("&harr;") + "&#10148;" +  "Заменить блюз на классика в средине масива"  + "<br/>" + "<hr/>");
 
+let leng = styles.length / 2;
+let lengMath = Math.floor(leng);
+document.write(styles[lengMath] + "&#10148;" +  "Находит средину в массиве любой длинны"  + "<br/>" + "<hr/>");
+
 let removed = styles.splice(0,1);
 document.write(removed + "&#10148;" +  "Удалите первый элемент масива и покажите его" + "<br/>" + "<hr/>");
  
 
 styles.unshift("Рэп", "Рэгги");
 document.write(styles.join("&harr;") + "&#10148;" +  "Вставьте рэп и рэгги в начало массива" + "<hr/>");
+
+let arr = ["a", "b", "c"];
+let arr1 = [1,2,3];
+
+let arr2 = arr.concat(arr1);
+document.write("1) " + arr + " + " + arr1 + " = " + arr2 + "<br/>" + "<hr/>");
+
+let arr3 = arr.push(1,2,3);
+document.write("2) " + arr + " 1,2,3 в конец масива" + "<br/>" + "<hr/>");
+
+document.write("<h3> Код будет дополнятся по мере виполнения доп. задания к домашке <h3/>")
+
